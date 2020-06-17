df3470a96b08ffc187e529ef0e4ab9a0f0a8a06b
fouz ayed <66705147+fouzm70@users.noreply.github.com>
2020-06-17 16:00:53 +0300

    done

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -7,19 +7,19 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+let Name = "Salem"
 
 
 var myWeight = 60
-var FriendWeight = 80.5
+var FriendWeight = Int (80.5)
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = myWeight + Int(FriendWeight)
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = myWeight * Int(height * height)
 
 /*
 - - - -
@@ -38,31 +38,40 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "fouz"
+var lastName1 = "ayed"
+var email1 = "fouzm70@gmail.com"
+var phoneNumber1 = "11111111"
+var age1 = 16
+var country1 = "kuwait"
+var area1 = "sulaibikhat"
+var password = "1234"
+var isKuwaiti = true
 
 //Person 2
-
-
-
-
-
-
+var friendfirstName2 = "wrood"
+var friendlastName2 = "alrashidi"
+var friendemail2 = "wrood@gmail.com"
+var friendphoneNumber2 = "22222222"
+var friendage2 = 17
+var friendcountry2 = "kuwait"
+var friendarea2 = "sulaibikhat"
+var friendpassword = "4321"
+var friendisKuwaiti = true
 
 
 
 
 //Person 3
-
-
-
+var friendfirstName3 = "ghadeer"
+var friendlastName3 = "almutairi"
+var friendemail3 = "ghadeer@gmail.com"
+var friendphoneNumber3 = "33333333"
+var friendage3 = 19
+var friendcountry3 = "kuwait"
+var friendarea3 = "jahra"
+var friendpassword3 = "54321"
+var friendisKuwaiti3 = true
 
 
 
@@ -82,3 +91,238 @@ var isKuwaiti = false
 
  
  */
+firstName1 > friendfirstName2
+lastName1 > friendlastName2
+email1 > friendemail2
+phoneNumber1 > friendphoneNumber2
+age1 > friendage2
+country1 > friendcountry2
+area1 > friendarea2
+password > friendpassword
+
+firstName1 < friendfirstName2
+lastName1 < friendlastName2
+email1 < friendemail2
+phoneNumber1 < friendphoneNumber2
+age1 < friendage2
+country1 < friendcountry2
+area1 < friendarea2
+password < friendpassword
+
+firstName1 >= friendfirstName2
+lastName1 >= friendlastName2
+email1 >= friendemail2
+phoneNumber1 >= friendphoneNumber2
+age1 >= friendage2
+country1 >= friendcountry2
+area1 >= friendarea2
+password >= friendpassword
+
+firstName1 <= friendfirstName2
+lastName1 <= friendlastName2
+email1 <= friendemail2
+phoneNumber1 <= friendphoneNumber2
+age1 <= friendage2
+country1 <= friendcountry2
+area1 <= friendarea2
+password <= friendpassword
+
+firstName1 == friendfirstName2
+lastName1 == friendlastName2
+email1 == friendemail2
+phoneNumber1 == friendphoneNumber2
+age1 == friendage2
+country1 == friendcountry2
+area1 == friendarea2
+password == friendpassword
+
+firstName1 != friendfirstName2
+lastName1 != friendlastName2
+email1 != friendemail2
+phoneNumber1 != friendphoneNumber2
+age1 != friendage2
+country1 != friendcountry2
+area1 != friendarea2
+password != friendpassword
+
+firstName1 > friendfirstName3
+lastName1 > friendlastName3
+email1 > friendemail3
+phoneNumber1 > friendphoneNumber3
+age1 > friendage3
+country1 > friendcountry3
+area1 > friendarea3
+password > friendpassword3
+
+firstName1 < friendfirstName3
+lastName1 < friendlastName3
+email1 < friendemail3
+phoneNumber1 < friendphoneNumber3
+age1 < friendage3
+country1 < friendcountry3
+area1 < friendarea3
+password < friendpassword3
+
+firstName1 >= friendfirstName3
+lastName1 >= friendlastName3
+email1 >= friendemail3
+phoneNumber1 >= friendphoneNumber3
+age1 >= friendage3
+country1 >= friendcountry3
+area1 >= friendarea3
+password >= friendpassword3
+
+firstName1 <= friendfirstName3
+lastName1 <= friendlastName3
+email1 <= friendemail3
+phoneNumber1 <= friendphoneNumber3
+age1 <= friendage3
+country1 <= friendcountry3
+area1 <= friendarea3
+password <= friendpassword3
+
+firstName1 == friendfirstName3
+lastName1 == friendlastName3
+email1 == friendemail3
+phoneNumber1 == friendphoneNumber3
+age1 == friendage3
+country1 == friendcountry3
+area1 == friendarea3
+password == friendpassword3
+
+firstName1 != friendfirstName3
+lastName1 != friendlastName3
+email1 != friendemail3
+phoneNumber1 != friendphoneNumber3
+age1 != friendage3
+country1 != friendcountry3
+area1 != friendarea3
+password != friendpassword3
+
+firstName1 > friendfirstName3
+lastName1 > friendlastName3
+email1 > friendemail3
+phoneNumber1 > friendphoneNumber3
+age1 > friendage3
+country1 > friendcountry3
+area1 > friendarea3
+password > friendpassword3
+
+firstName1 < friendfirstName3
+lastName1 < friendlastName3
+email1 < friendemail3
+phoneNumber1 < friendphoneNumber3
+age1 < friendage3
+country1 < friendcountry3
+area1 < friendarea3
+password < friendpassword3
+
+firstName1 >= friendfirstName3
+lastName1 >= friendlastName3
+email1 >= friendemail3
+phoneNumber1 >= friendphoneNumber3
+age1 >= friendage3
+country1 >= friendcountry3
+area1 >= friendarea3
+password >= friendpassword3
+
+firstName1 <= friendfirstName3
+lastName1 <= friendlastName3
+email1 <= friendemail3
+phoneNumber1 <= friendphoneNumber3
+age1 <= friendage3
+country1 <= friendcountry3
+area1 <= friendarea3
+password <= friendpassword3
+
+firstName1 == friendfirstName3
+lastName1 == friendlastName3
+email1 == friendemail3
+phoneNumber1 == friendphoneNumber3
+age1 == friendage3
+country1 == friendcountry3
+area1 == friendarea3
+password == friendpassword3
+
+firstName1 != friendfirstName3
+lastName1 != friendlastName3
+email1 != friendemail3
+phoneNumber1 != friendphoneNumber3
+age1 != friendage3
+country1 != friendcountry3
+area1 != friendarea3
+password != friendpassword3
+
+friendfirstName2 > friendfirstName3
+friendlastName2 > friendlastName3
+friendemail2 > friendemail3
+friendphoneNumber2 > friendphoneNumber3
+friendage2 > friendage3
+friendcountry2 > friendcountry3
+friendarea2 > friendarea3
+friendpassword > friendpassword3
+
+friendfirstName2 < friendfirstName3
+friendlastName2 < friendlastName3
+friendemail2 < friendemail3
+friendphoneNumber2 < friendphoneNumber3
+friendage2 < friendage3
+friendcountry2 < friendcountry3
+friendarea2 < friendarea3
+friendpassword < friendpassword3
+
+friendfirstName2 >= friendfirstName3
+friendlastName2 >= friendlastName3
+friendemail2 >= friendemail3
+friendphoneNumber2 >= friendphoneNumber3
+friendage2 >= friendage3
+friendcountry2 >= friendcountry3
+friendarea2 >= friendarea3
+friendpassword >= friendpassword3
+
+friendfirstName2 <= friendfirstName3
+friendlastName2 <= friendlastName3
+friendemail2 <= friendemail3
+friendphoneNumber2 <= friendphoneNumber3
+friendage2 <= friendage3
+friendcountry2 <= friendcountry3
+friendarea2 <= friendarea3
+friendpassword <= friendpassword3
+
+friendfirstName2 == friendfirstName3
+friendlastName2 == friendlastName3
+friendemail2 == friendemail3
+friendphoneNumber2 == friendphoneNumber3
+friendage2 == friendage3
+friendcountry2 == friendcountry3
+friendarea2 == friendarea3
+friendpassword == friendpassword3
+
+friendfirstName2 != friendfirstName3
+friendlastName2 != friendlastName3
+friendemail2 != friendemail3
+friendphoneNumber2 != friendphoneNumber3
+friendage2 != friendage3
+friendcountry2 != friendcountry3
+friendarea2 != friendarea3
+friendpassword != friendpassword3
+
+//person1 compare
+age1 > 18
+age1 < 18
+age1 == 17
+age1 != 17
+
+//person2 compare
+friendage2 > 18
+friendage2 < 18
+friendage2 == 17
+friendage2 != 17
+
+//person3 compare
+friendage3 > 18
+friendage3 < 18
+friendage3 == 17
+friendage3 != 17
+
+
